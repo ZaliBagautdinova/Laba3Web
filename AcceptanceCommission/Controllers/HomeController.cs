cd3032478e73b5be4bee458dbec4657fb737c9e5
Budda <Budda@DESKTOP-AJ4TM78>
2018-10-21 22:00:51 +0300

    Добавьте файлы проекта.

---
+++ AcceptanceCommission/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcceptanceCommission.Models;
+
+namespace AcceptanceCommission.Controllers
+{
+    public class HomeController : Controller
+    {
+        AScontext db = new AScontext();
+
+        public ActionResult Index()
+        {
+            
+            return View(db.Students);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AcceptanceCommission/Models/AScontext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AcceptanceCommission.Models
+{
+    public class AScontext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
